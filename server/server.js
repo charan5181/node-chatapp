2fc93777f1db6280796304e25c10ff36132192c7
R SRI CHARAN REDDY <charanreddy.5181@gmail.com>
2018-09-08 22:31:35 +0530

    Added css to Chat page

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
 const path = require('path');
+var http = require('http');
+const socketIO = require('socket.io');
+const {
+  generatemessage,
+  generatelocationmessage
+} = require('./utils/message');
 
 //using express middleware
 const express = require('express');
@@ -9,10 +15,38 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(publicPath));
+var server = http.createServer(app);
+//thorugh io we can emit or listen events of web sockets
+var io = socketIO(server);
+
+//Code Goes here
+//Helps to register on event listener
+io.on('connection', (socket) => {
+  console.log('New user Connected');
+
+  socket.emit('newmessage', generatemessage('Admin', 'Welcome to the chat'));
+
+  //listener
+  socket.on('createmessage', (message, callback) => {
+    console.log(message);
+    //io.emit will send data to all users & socket.emit to particular user
+    io.emit('newmessage', generatemessage(message.from, message.text));
+    callback();
+
+  });
+
+  socket.on('createlocationmessage', (coords) => {
+    io.emit('newlocationmessage', generatelocationmessage('Admin', coords.latitude, coords.longitude))
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 
 
 //app will listen on port localhost port 3000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on ${port}...`);
 });
---
+++ server/utils/message.js
@@ -0,0 +1,19 @@
+var generatemessage = function(from, text) {
+  return {
+    from,
+    text,
+    createdAt: new Date().getTime()
+  };
+};
+
+var generatelocationmessage = function(from,lat,long){
+  return{
+    from,
+    url:`https://www.google.com/maps?q=${lat},${long}`,
+    createdAt: new Date().getTime()
+  };
+};
+
+module.exports = {
+  generatemessage,generatelocationmessage
+};
